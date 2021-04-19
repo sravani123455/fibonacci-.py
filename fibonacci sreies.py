f55ea93d39492da0668037fe0c5ec7ea1eb797c8
sravani123455 <82307612+sravani123455@users.noreply.github.com>
2021-04-19 10:44:18 +0530

    Create fibonacci sreies.py
    it is a mathematical calculation of finding fibonacci series
    how to calculate:
    n1=0
    n2=1
    n3= n1+n2
    n3=1

---
+++ fibonacci sreies.py
@@ -0,0 +1,19 @@
+nterms=int(input("how many terms you want?"))
+n1=0
+n2=1
+count=0
+if nterms <= 0:
+    print("please enter a positive integer:")
+elif nterms == 1:
+    print("fibonacci sequence upto",nterms,".")
+    print(n1)
+else:
+    print("fibonacci sequence:")
+    print(n1)
+while count<nterms:
+     print(n1)
+     nth=n1+n2
+     n1=n2
+     n2=nth
+     count += 1
+                
